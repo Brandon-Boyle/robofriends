87a5c9ec18f124f5629ea35f770cb75b2026a5ee
Brandon Boyle <brandonboyle74@yahoo.com>
2022-09-18 17:43:05 -0400

    test me

---
+++ src/robots.js
@@ -0,0 +1,47 @@
+export const robots = [
+    {
+        id: 1,
+        username: 'Gabriela',
+        email: 'gabriela@email.com'
+    },
+    {
+        id: 2,
+        username: 'Bob',
+        email: 'bob@email.com'
+    },
+    {
+        id: 3,
+        username: 'Suzy',
+        email: 'suzy@email.com'
+    },
+    {
+        id: 4,
+        username: 'Kane',
+        email: 'email@email.com'
+    },
+    {
+        id: 5,
+        username: 'Steve',
+        email: 'steve@email.com'
+    },
+    {
+        id: 6,
+        username: 'Emily',
+        email: 'emily@email.com'
+    },
+    {
+        id: 7,
+        username: 'Bruce',
+        email: 'bruce@email.com'
+    },
+    {
+        id: 8,
+        username: 'Devin',
+        email: 'devin@email.com'
+    },
+    {
+        id: 9,
+        username: 'gGorge',
+        email: 'george@email.com'
+    }
+]
